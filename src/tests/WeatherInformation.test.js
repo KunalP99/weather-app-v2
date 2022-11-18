c3479ed31704576f16c91a93aeb07a2957014320
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-11-18 17:37:03 +0000

    Test for WeatherInformation complete

--- src/tests/WeatherInformation.test.js
+++ src/tests/WeatherInformation.test.js
@@ -1,25 +1,36 @@
 import React from "react";
-import {
-  render,
-  screen,
-  cleanup,
-  waitForElement,
-  queryByTestId,
-} from "@testing-library/react";
+import { render, screen, cleanup, waitFor } from "@testing-library/react";
+import axiosMock from "axios";
 import WeatherInformation from "../components/WeatherInformation";
 
 afterEach(cleanup);
 
 describe("WeatherInformation", () => {
-  it("should render search box", () => {
+  it("should render search box", async () => {
     render(<WeatherInformation />);
     const searchBox = screen.getByPlaceholderText("Type city name:");
-    expect(searchBox).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(searchBox).toBeInTheDocument();
+    });
   });
 
   it("should not be rendered when waiting for data", async () => {
     const { queryByTestId } = render(<WeatherInformation />);
 
-    expect(queryByTestId(/weather-component/i)).toBeNull();
+    await waitFor(() => {
+      expect(queryByTestId(/weather-component/i)).toBeNull();
+    });
+  });
+
+  it("should render weather component when api is called", async () => {
+    const { queryByTestId } = render(<WeatherInformation />);
+    axiosMock.get.mockResolvedValueOnce({ location: {} });
+
+    await waitFor(() => {
+      expect(axiosMock.get).toHaveBeenCalled();
+      expect(queryByTestId(/weather-component/i)).toHaveTextContent("London");
+    });
+    screen.debug();
   });
 });
